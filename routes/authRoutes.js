1b587337550c2743a93b5ec3de6d8474524ff177
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2024-07-11 22:42:33 +0200

    get users route added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -47,4 +47,17 @@ router.post('/login', async (req, res) => {
     }
 });
 
+
+//get all users
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find().select('-password'); // Exclude password
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error.' });
+    }
+});
+
+
 export default router;
